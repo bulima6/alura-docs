b4fd706570cba299166908aca8da6a763afe1b9a
bulima6 <bumagno@gmail.com>
2023-06-16 15:23:42 -0300

    fourth commit

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,8 +1,15 @@
-import registrarEventosDocumento from "./registrarEventos/registrarEventosDocumento.js";
-import registrarEventosInicio from "./registrarEventos/registrarEventosInicio.js";
+import "dotenv/config";
+
+import registrarEventosDocumento from "./registrarEventos/documento.js";
+import registrarEventosInicio from "./registrarEventos/inicio.js";
+import registrarEventosCadastro from "./registrarEventos/cadastro.js";
+import registrarEventosLogin from "./registrarEventos/login.js";
+
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
+  registrarEventosCadastro(socket, io);
   registrarEventosInicio(socket, io);
+  registrarEventosLogin(socket, io);
   registrarEventosDocumento(socket, io);
 });
